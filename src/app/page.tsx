1928d4314161a2a3d4a415ae76cec88323ceb1fb
teceer <wchudzikowski@gmail.com>
2023-11-29 21:47:27 +0100

    Remove AddEvent component from Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
 import Ticket from "~/components/Ticket/Ticket";
-import AddEvent from "~/components/global/Event/AddEvent";
-import EventCard from "~/components/global/Event/EventCard";
 import Events from "~/components/global/Event/Events";
 import Section from "~/components/global/Section";
 
 export default function Home() {
   return (
     <Section DivClassName="space-y-4">
-      <AddEvent />
       <div className="grid gap-4 sm:grid-cols-2 ">
         <Events />
         <Ticket code="ABCD12345" />
