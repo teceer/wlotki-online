188330ede25fa2c7981e04ce162379ef5c9f781c
teceer <wchudzikowski@gmail.com>
2023-12-12 23:33:14 +0100

    Fix bug in login functionality

--- src/app/dashboard/[[...slug]]/Settings.tsx
+++ src/app/dashboard/[[...slug]]/Settings.tsx
@@ -1,7 +1,6 @@
 "use client";
-import Spline from "@splinetool/react-spline";
 import { Coins, Mail } from "lucide-react";
-import { Session } from "next-auth";
+import type { Session } from "next-auth";
 import { useRouter } from "next/navigation";
 import React from "react";
 import { toast } from "react-toastify";
