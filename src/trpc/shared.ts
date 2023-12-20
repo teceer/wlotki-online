b3eabb8d92ba5498c3519704dc11f0efa33f1a70
teceer <wchudzikowski@gmail.com>
2023-12-20 03:29:15 +0100

    Add support for NEXTAUTH_URL environment variable
    This commit adds support for the `NEXTAUTH_URL` environment variable in the `getBaseUrl()` function. This allows the application to use the value of `NEXTAUTH_URL` if it is defined, providing flexibility in configuring the base URL.

--- src/trpc/shared.ts
+++ src/trpc/shared.ts
@@ -7,6 +7,7 @@ export const transformer = superjson;
 
 function getBaseUrl() {
   if (typeof window !== "undefined") return "";
+  if (process.env.NEXTAUTH_URL) return process.env.NEXTAUTH_URL;
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
   return `http://localhost:${process.env.PORT ?? 3000}`;
 }
