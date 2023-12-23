f8f371b62b57ff9df1593d7d2323399d0c0a1abe
teceer <wchudzikowski@gmail.com>
2023-12-23 05:45:23 +0100

    Refactor EventHeader component styling

--- src/components/Event/EventHeader.tsx
+++ src/components/Event/EventHeader.tsx
@@ -18,14 +18,19 @@ export default function EventHeader({
             {image && (
               <Image alt="" src={image} fill className="object-cover" />
             )}
-            <div className="absolute h-full w-full bg-gradient-to-t from-background backdrop-blur-sm" />
+            <div className="absolute h-full w-full bg-gradient-to-b from-background to-background/30 backdrop-blur-sm" />
           </div>
         </div>
       </div>
-      <Section DivClassName="z-10">
-        <div className="flex items-end justify-between gap-4">
-          <H1 className="pb-8 md:pb-12">{title}</H1>
-          <div className="relative aspect-square w-40 shrink-0 md:w-64">
+      <Section
+        DivClassName="z-10 flex flex-col max-w-full"
+        SectionClassName="pt-0 pl-0 md:pl-0 md:py-0"
+      >
+        <div className="flex h-full grow justify-between gap-4">
+          <div className="flex h-max items-end rounded-br-lg border-b border-r bg-background/70 p-4 backdrop-blur md:pl-36 md:pr-8 md:pt-8">
+            <H1>{title}</H1>
+          </div>
+          <div className="relative mt-[18px] aspect-square w-28 shrink-0 rotate-6 md:mt-8 md:w-64">
             {image && (
               <>
                 <Image
@@ -38,7 +43,7 @@ export default function EventHeader({
                   alt=""
                   src={image}
                   fill
-                  className="rounded-lg object-cover"
+                  className="rounded-lg border object-cover"
                 />
               </>
             )}
