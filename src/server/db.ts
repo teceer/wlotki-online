2b46c2bdb20015e1cf5994624a82cc40e03c6d8c
teceer <wchudzikowski@gmail.com>
2023-12-24 03:10:12 +0100

    Commented out unused code in db.ts

--- src/server/db.ts
+++ src/server/db.ts
@@ -3,15 +3,15 @@ import { dropExtender } from "prisma/extenders";
 
 import { env } from "~/env.mjs";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient; // | undefined
-};
+// const globalForPrisma = globalThis as unknown as {
+//   prisma: PrismaClient; // | undefined
+// };
 
 export const db =
-  globalForPrisma.prisma ??
+  // globalForPrisma.prisma ??
   new PrismaClient({
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   }).$extends(dropExtender);
 
-if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+// if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
